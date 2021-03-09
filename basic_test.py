ab4acf5d1299b08b4999d36e2030d5130c6d79c5
Dmitry A. Fedorov <dfedorov@nevada.unr.edu>
2021-03-09 15:20:28 -0600

    Cleaned up

--- basic_test.py
+++ basic_test.py
@@ -5,20 +5,25 @@ from qiskit.aqua.algorithms import NumPyMinimumEigensolver
 from qiskit import Aer
 from qiskit.aqua import QuantumInstance
 from qiskit.chemistry.algorithms.ground_state_solvers.minimum_eigensolver_factories import VQEUCCSDFactory
+from qiskit.chemistry.components.variational_forms import UCCSD
 from qiskit.aqua.algorithms import VQE
 from qiskit.chemistry.algorithms.ground_state_solvers import GroundStateEigensolver
 from qiskit.aqua.operators import I, X, Z
+from qiskit.chemistry.circuit.library.initial_states import HartreeFock
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 def construct_jastrow_factor(num_qubits : int,
                              alpha: np.array,
                              beta: np.array):
-
+    """Construction of Jastrow operator as in arXiv:2101.09316v1, equation 9"""
+    
     # construct jastrow factor
     jastrow_factor = (1*I)
     for i in range(num_qubits-1):
         jastrow_factor = jastrow_factor^I
     
-    print("Number of qubits is {}".format(num_qubits))
     # First term construction
     for i in range(num_qubits):
         for k in range(num_qubits):
@@ -59,40 +64,52 @@ molecule = Molecule(geometry=[['H', [0., 0., 0.]],
 driver = PySCFDriver(molecule = molecule, unit=UnitsType.ANGSTROM, basis='sto3g')
 transformation = FermionicTransformation(qubit_mapping=FermionicQubitMappingType.JORDAN_WIGNER, two_qubit_reduction=False)
 
+qubit_ham = transformation.transform(driver)
 # numpy_solver = NumPyMinimumEigensolver()
 # calc = GroundStateEigensolver(transformation, numpy_solver)
 # res = calc.solve(driver)
 # print(res)
 
-vqe_solver = VQEUCCSDFactory(QuantumInstance(Aer.get_backend('qasm_simulator')))
-calc = GroundStateEigensolver(transformation, vqe_solver)
+num_orbitals = transformation.molecule_info['num_orbitals']
+num_particles = transformation.molecule_info['num_particles']
+qubit_mapping = transformation.qubit_mapping
+two_qubit_reduction = transformation.molecule_info['two_qubit_reduction']
+z2_symmetries = transformation.molecule_info['z2_symmetries']
+
+initial_state = HartreeFock(num_orbitals, num_particles, qubit_mapping,
+                                    two_qubit_reduction, z2_symmetries.sq_list)
+
+var_form = UCCSD(num_orbitals=num_orbitals,
+                 num_particles=num_particles,
+                 initial_state=initial_state,
+                 qubit_mapping=qubit_mapping,
+                 two_qubit_reduction=two_qubit_reduction,
+                 z2_symmetries=z2_symmetries)
+
+vqe = VQE(var_form=var_form, quantum_instance=QuantumInstance(Aer.get_backend('qasm_simulator')))
+calc = GroundStateEigensolver(transformation, vqe)
 res = calc.solve(driver)
 # print(res)
 
-num_parameters = vqe_solver._vqe.var_form.num_parameters
+num_parameters = vqe.var_form.num_parameters
 print("Number of parameters:", num_parameters)
 
 seed = np.random.seed(43)
 parameters = np.random.rand(num_parameters)
 
-# circuit = vqe_solver._vqe.var_form.construct_circuit(parameters)
-# print(circuit.decompose())
-
+circuit = vqe.var_form.construct_circuit(parameters)
+print("Parameters", parameters)
+print("Circuit\n", circuit.decompose())
 
 # This is the qubit Hamiltonian consisting of Pauli strings with coefficients in front
 qubit_ham = calc.transformation.transform(driver)
 num_qubits = qubit_ham[0].num_qubits
+print("Number of qubits is {}".format(num_qubits))
 print("Qubit Hamiltonian")
 print(qubit_ham[0])
 
+# Constructing Jastrow factor
 alpha = np.random.rand(num_qubits)
 beta = np.random.rand(num_qubits, num_qubits)        
 jastrow_factor = construct_jastrow_factor(num_qubits, alpha, beta)
-
-# print("\nqubit_op ")
-# for op in vqe_solver._vqe.var_form._hopping_ops[0].paulis:
-#     print(op)
 print("\njastrow\n", jastrow_factor)
-# print("\nproduct\n", jastrow_factor@vqe_solver._vqe.var_form._hopping_ops[0])
-# print("\nproduct\n", jastrow_factor@vqe_solver._vqe.var_form._hopping_ops[0].paulis[0][1])
-# print("\nproduct\n", (jastrow_factor@vqe_solver._vqe.var_form._hopping_ops[0].paulis[1][1]))
